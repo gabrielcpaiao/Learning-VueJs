c58ce24ebffb4abb5c494a362621121d0f344ce6
Gabriel Paião <costapaiao@hotmail.com>
2025-01-29 01:02:48 -0300

    :sparkles: feat: Learning about if statments, loop, function methods and forms with VueJs

---
+++ Step2/vueJsCourseNpm/src/App2.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Gabriel',
+      status: 'pending',
+      tasks: ['Task one', 'Task two', 'Task three'],
+      link: 'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Hello, World!</h1>
+  <p>{{ name }}</p>
+
+  <p v-if="status === 'active' ">User is active</p>
+  <p v-else-if="status === 'pending' ">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <br>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a v-bind:href="link">Click for Google</a>
+
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+
+</template>
